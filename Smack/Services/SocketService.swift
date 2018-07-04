854b135726dd0a65e7292866d379b540ded5791f
Zeynal Zeynalov <Zeyn@Zeynals-MacBook-Air.local>
2018-07-04 19:55:11 +0400

    lesson-22

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,58 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Zeynal Zeynalov on 7/4/18.
+//  Copyright © 2018 Zeynal Zeynalov. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    
+    var manager: SocketManager = SocketManager(socketURL: URL(string: BASE_URL)!)
+//    var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    
+    var socket : SocketIOClient = SocketIOClient(manager: SocketManager(socketURL: URL(string: BASE_URL)!), nsp: BASE_URL)
+
+    func establishConnection() {
+        socket.connect()
+    }
+
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
